4aafccca938169213288334c6ac48558fa90eea0
BlackCritical <lvlup@t-online.de>
2017-02-14 17:32:38 +0100

    The Start

--- app/src/main/java/com/example/leander/saufkomatiko/MainActivity.java
+++ app/src/main/java/com/example/leander/saufkomatiko/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.leander.saufkomatiko;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void addPlayer(View v){
+        EditText textBox = (EditText) v.findViewById(R.id.textBox);
+        //ListView listView = (ListView) v.findViewById(R.id.textView);
+
+
+    }
+
 }
