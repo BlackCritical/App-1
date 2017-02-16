89ef554d14a1959764a01e3dec2a987a948380ee
Kriller <lvlup@t-online.de>
2017-02-16 06:18:54 +0100

    Big Patch

--- app/src/main/java/com/example/leander/saufkomatiko/MainActivity.java
+++ app/src/main/java/com/example/leander/saufkomatiko/MainActivity.java
@@ -1,29 +1,20 @@
 package com.example.leander.saufkomatiko;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public ArrayList<String> list = new ArrayList<>();
     public ArrayAdapter<String> adap;
-    public String[] arrayS = {"hi", "ho", "hey", "lo0", "tooooo"};
-    public int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,56 +23,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGame(View v){
-        Intent intent = new Intent(this, StartGameActivity.class);
-        intent.putExtra("players", list);
-        startActivity(intent);
+        if(list.isEmpty())
+            Toast.makeText(this, "You need at least one Player", Toast.LENGTH_SHORT).show();
+        else {
+            Intent intent = new Intent(this, StartGameActivity.class);
+            intent.putExtra("players", list);
+            startActivity(intent);
+        }
     }
 
     public void addPlayer(View v){
-        EditText textBox = (EditText) findViewById(R.id.textBox);
-        ListView listView = (ListView) findViewById(R.id.textView);
+        EditText textBox = (EditText) findViewById(R.id.nameField);
+        ListView listView = (ListView) findViewById(R.id.playerList);
+        String name = textBox.getText().toString();
 
-        list.add(textBox.getText().toString());
+        if (name.length() > 1) {
+            list.add(name);
+            adap = new ArrayAdapter<>(this, R.layout.listlayout, list);
+            listView.setAdapter(adap);
 
-        adap = new ArrayAdapter<>(this, R.layout.listlayout, list);
-        listView.setAdapter(adap);
-
-        Toast.makeText(this, "Name Added " + textBox.getText().toString(), Toast.LENGTH_SHORT).show();
-        textBox.setText("");
-    }
-
-    public void coolerClicker(View v){
-        EditText editBox = (EditText) findViewById(R.id.textBox);
-        ListView listView = (ListView) findViewById(R.id.textView);
-
-        list.add(count, editBox.getText().toString());
-        count++;
-
-        MeinAdapterino ada = new MeinAdapterino(this, R.layout.stackoverflow, list);
-        listView.setAdapter(ada);
-
-        Toast.makeText(this, "Name Added " + editBox.getText().toString(), Toast.LENGTH_SHORT).show();
-        editBox.setText("");
-    }
-
-    private class MeinAdapterino extends ArrayAdapter<String> {
-
-        public MeinAdapterino(Context context, int resource, List<String> objects) {
-            super(context, resource, objects);
-        }
-
-
-        @NonNull
-        @SuppressLint("ViewHolder")
-        @Override
-        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
-            convertView = getLayoutInflater().inflate(R.layout.stackoverflow, null);
-            String m = getItem(position);
-            TextView textView = (TextView) convertView.findViewById(R.id.id);
-            if (m != null) {
-                textView.setText(m);
-            }
-            return convertView;
+            Toast.makeText(this, "The Player " + name + " was added!", Toast.LENGTH_SHORT).show();
+            textBox.setText("");
+        }else{
+            Toast.makeText(this, "The name has to be at least 2 characters long!", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/leander/saufkomatiko/StartGameActivity.java
+++ app/src/main/java/com/example/leander/saufkomatiko/StartGameActivity.java
@@ -1,9 +1,19 @@
 package com.example.leander.saufkomatiko;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
+
+import java.util.ArrayList;
 
 /**
  * Created by Leander on 15.02.2017.
@@ -11,13 +21,69 @@ import android.widget.ImageView;
 
 public class StartGameActivity extends AppCompatActivity{
 
+    private ImageSwitcher imgSw;
+    private int[] images = {R.drawable.android_device_frame_land, R.drawable.phoneportraitvlandscape,
+            R.drawable.pic, R.drawable.code};
+    private int[] colors = {R.color.colorAccent, R.color.colorPrimary, R.color.colorPrimaryDark};
+    private int random;
+    private int last;
+    private boolean isPrevious = false;
+    private Animation in;
+    private Animation out;
+    private ArrayList<String> players;
+    private int i = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.task);
-        ImageView imageView = (ImageView) findViewById(R.id.taskImages);
-        imageView.setImageResource(R.drawable.ic_test);
-        setContentView(R.layout.task);
+        setContentView(R.layout.tasks);
+        imgSw = (ImageSwitcher) findViewById(R.id.imageSw);
+        imgSw.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                return imageView;
+            }
+        });
+
+        in = AnimationUtils.loadAnimation(this, R.anim.in);
+        out = AnimationUtils.loadAnimation(this, R.anim.out);
+        imgSw.setInAnimation(in);
+        imgSw.setOutAnimation(out);
+
+        Intent intent = getIntent();
+        players = intent.getStringArrayListExtra("players");
+    }
+
+    public void previous(View v){
+//        imgSw.setInAnimation(out);
+//        imgSw.setOutAnimation(in);
+        random = last;
+        if (isPrevious)
+            Toast.makeText(this, "You can only go back once!", Toast.LENGTH_SHORT).show();
+        else {
+            imgSw.setImageResource(images[last]);
+            isPrevious = true;
+        }
+    }
+
+    public void next(View v){
+//        imgSw.setInAnimation(in);
+//        imgSw.setOutAnimation(out);
+        TextView displayTask = (TextView) findViewById(R.id.displayTask);
+        displayTask.bringToFront();
+        if(i > players.size() - 1)
+            i = 0;
+        displayTask.setText("\nHello,\n" + players.get(i++) +
+                "\n you have to fuck your neighbour, or drink 23 shots!");
+
+        isPrevious = false;
+        last = random;
+        while(random == last)
+            random = (int) (Math.random() * colors.length);
+        //imgSw.setBackgroundColor(colors[random]);
+        imgSw.setImageResource(images[last]);
     }
 
 }
