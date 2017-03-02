b5d0be42bbd751e013529682df26ce895829fd5b
BlackCritical <lvlup@t-online.de>
2017-03-02 17:08:11 +0100

    next Activity

--- app/src/main/java/com/example/leander/saufkomatiko/activitys/ChooseGameModeActivity.java
+++ app/src/main/java/com/example/leander/saufkomatiko/activitys/ChooseGameModeActivity.java
@@ -3,6 +3,10 @@ package com.example.leander.saufkomatiko.activitys;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.leander.saufkomatiko.R;
 
 /**
  * Created by Leander on 02.03.2017.
@@ -13,6 +17,15 @@ public class ChooseGameModeActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.chooser);
 
     }
 
